4772ab5f4753083bdca2af1521192329dd529384
Ramadhani19 <ramadhan@shuledirect.ac.tz>
2020-08-17 05:54:04 -0700

    Update Admin Records

--- app/Http/Controllers/MeetingsController.php
+++ app/Http/Controllers/MeetingsController.php
@@ -160,6 +160,40 @@ class MeetingsController extends Controller
         //
     }
 
+    public function uploadFile(Request $request, Meeting $meeting)
+    {
+        if($request->hasFile('file-upload')){
+            $dir = 'public/documents/';
+            $filetype = $request->input('file-type');
+            $ext = $request->file('file-upload')->extension();
+
+            $path = $request->file('file-upload')->store($dir.$filetype);
+
+            if($path !== null){
+                $document = $this->create_document([
+                    "type" => $filetype,
+                    "path" => $path,
+                    "extension" => $ext]);
+                
+                if($meeting->ofDepartment()){
+                    $departmentMeeting = DepartmentMeeting::where('meeting_id',$meeting->id)
+                                        ->where(function($query){
+                                                $query->where('department_id',Auth::User()->department_id);
+                                        })->get();
+                    $departmentMeeting->first()->documents()->save($document);
+                }elseif ($meeting->ofDirectorate()) {
+                    # code...
+                }elseif ($meeting->ofSchool()) {
+                    # code...
+                }elseif ($meeting->ofCommittee()) {
+                    # code...
+                }
+            }
+        }
+
+        return back();
+    }
+
     public function downloadFile(Request $request)
     {
         $document = $request->input('document');
@@ -247,6 +281,16 @@ class MeetingsController extends Controller
                 "meeting_time" => $meeting['time'],
             )
         );
+    }
+
+    protected function create_document($docInfo){
+        $document = new Document([
+            'document_name' => "",
+            'document_type' => $docInfo["type"],
+            'document_url' => $docInfo["path"],
+            'document_extension' => $docInfo["extension"]
+        ]);
+        return $document;
     }
 
 }
